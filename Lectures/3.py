19c5b176f35fda60e80ecee20ec2bf15f49c9850
Andrii Khymera <andriikhymera@gmail.com>
2017-12-05 20:38:22 +0200

    Lecture

--- Lectures/3.py
+++ Lectures/3.py
@@ -1,10 +1,42 @@
 import requests
+import  time
+import random
+
+# def print_current_time():
+
+def get_request_to(URL):
+    return requests.get(URL)
+
 
 def main():
-    res = requests.get("http://google.com")
-    print (res.text)
+    for i in range(0, 5):
+        time.sleep(random.randint(0, 5))
+        print (get_request_to("http://google.com"))
 
-main()
+def checKArray(*URL_array):
+    for url in URL_array:
+        try:
+            res = get_request_to(url)
+            if(res.status_code == 200):
+                print ("url: {} is okay".format(url))
+        except:
+            print ("url: {} is bad".format(url))
+
+def print_file(path_to_file):
+    with open(path_to_file, "r") as file:
+        print (file.read())
+
+
+URL_ARR={
+    "http://google.com",
+    "http://wikipedia.org",
+    "http://qealksdjlasj.com",
+    "http://yahoo.com"
+}
+
+print_file("3_lecture_json_example.json")
+# checKArray(*URL_ARR)
+# main()
 
 # if __name__ == "__main__":
 #     main()
